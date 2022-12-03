075c9e83065387c2bafb1808171fcc9328200dbd
Alexei Humeniy <yo@ahumeniy.net>
2022-12-02 23:11:09 -0300

    Rudimentary frontend/catalog website

---
+++ catalog/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Flipper Catalog</h1>
+      </header>
+      <content>
+        <p>Here's the list of apps</p>
+        <div>
+          
+        </div>
+      </content>
+    </div>
+  );
+}
+
+export default App;
